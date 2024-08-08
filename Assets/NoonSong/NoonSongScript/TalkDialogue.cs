271520c2f64868ec9b1d3dd1aafa1583709c783e
day-e-on <2016508@sookmyung.ac.kr>
2024-08-08 18:46:58 +0900

    애니메이션 수정, 연결 코드 수정
    notice: 눈덩이의 standing 애니메이션이 너무 길어서 스크립트를 다 읽어도 런타임이 길어요, 짧게 조정하면 더 좋겠습니다.
    
    눈꽃송이 loop로 idle 만들습니다.

--- Assets/NoonSong/NoonSongScript/TalkDialogue.cs
+++ Assets/NoonSong/NoonSongScript/TalkDialogue.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Doublsb.Dialog;
 
-public class TalkDialogue : MonoBehaviour
+public class TalkDialogue : MonoBehaviour // TalkDialogue는 튜토리얼 전체 대사가 들어있음
 {
-    public DialogManager DialogManager;
-    public Animator noonDungAnimator; // NoonDung 오브젝트의 Animator
-    public Animator RoroAnimator; // Roro 오브젝트의 Animator
+    public DialogManager DialogManager; // DialogManager 스크립트를 참조해서 함수사용함.
 
+    // 모든 animation들은 standing, fast, mover 가 trigger로 animator에서 전환 가능하게 만듦, 기본 애니메이션은 Idle로 설정함.
+    public Animator noonDungAnimator;  // NoonDung 오브젝트의 Animator 
+    public Animator snowflakeAnimator; // snowflake 오브젝트의 Animator
+    public Animator roroAnimator;      // roro 오브젝트의 Animator
+    public Animator noonkyeolAnimator; // noonkyeol 오브젝트의 Animator
+    public Animator kkotsongAnimator;  // kkotsong 오브젝트의 Animator
+    public Animator noonsongAnimator;  // noonsong 오브젝트의 Animator
 
-    public GameObject noonDung; // part1 눈덩이 등장
-    public GameObject noonkkot; // part2 눈꽃송이 등장
-    public GameObject roro; // part3 로로 등장
+    public GameObject noonDung;  // part1 눈덩이 등장
+    public GameObject snowflake; // part2 눈꽃송이 등장
+    public GameObject roro;      // part3 로로 등장
     public GameObject noonkyeol; // part4 눈결이 등장
-    public GameObject kkosong; // part5 꽃송이 등장
-    public GameObject noonsong; // part6 눈송이 등장
-    public GameObject turi; // part7 튜리 등장
+    public GameObject kkotsong;  // part5 꽃송이 등장
+    public GameObject noonsong;  // part6 눈송이 등장
 
     public Transform arCamera; // AR 카메라 Transform
     public float moveDuration = 2f; // 이동 애니메이션 지속 시간
@@ -26,44 +30,47 @@ public class TalkDialogue : MonoBehaviour
     private float elapsedTime = 0f;
     private bool isMoving = false;
 
-    // 첫 번째 대화 설정
+    // 첫 번째 대화 데이터 순서
     private void FirstDialog()
     {
         var FirstDialog = new List<DialogData>();
 
+        // part1 눈덩이 등장
         FirstDialog.Add(new DialogData("/color:black/숙명여대에 갓 입학한 새송이는 학교 탐방을 오게 되었다!"));
         FirstDialog.Add(new DialogData("/color:black/그런데 어쩌지? 학교가 너무 복잡해!"));
-        FirstDialog.Add(new DialogData("/color:black/[학교가 너무 처음이라 막막하네...]", "User", () => StartCoroutine(NoonDungComing()))); // 애니메이션 실행 및 대사 출력
+        FirstDialog.Add(new DialogData("/color:black/[학교가 너무 처음이라 막막하네...]", "User", () => StartCoroutine(NoonDungMove()))); // 3d 오브젝트 화면상에서 움직이는 코드
         FirstDialog.Add(new DialogData("/color:black//wait:0.5/안녕, 친구야! 혹시 무슨 고민 있어?", "NoonDung"));
-        FirstDialog.Add(new DialogData("/color:black/[(사정을 설명한다.)]", "User"));
-        FirstDialog.Add(new DialogData("/color:black/아하, 아직 학교가 처음이라 모르는 게 많다고? 음.. 어디보자~", "NoonDung", () => noondungChangeAnimation("stand")));
+        FirstDialog.Add(new DialogData("/color:black/[(사정을 설명한다.)]", "User", () => ChangeAnimation(noonDungAnimator, "standing"))); // 원하는 애니메이션은 바로 전 대사 뒤에 붙여야 자연스레 이어짐.
+        FirstDialog.Add(new DialogData("/color:black/아하, 아직 학교가 처음이라 모르는 게 많다고? 음.. 어디보자~", "NoonDung"));
         FirstDialog.Add(new DialogData("/color:black/그렇지! 숙명여대라면 역시 눈송이! 그 애가 널 도와줄 수 있을 거야!", "NoonDung"));
         FirstDialog.Add(new DialogData("/color:black/같이 학교를 돌아다니면서 /color:blue/눈송이/color:black/가 어디에 있는지 찾아보자!/wait:1//close/", "NoonDung"));
         // part2 눈꽃송이 등장
         FirstDialog.Add(new DialogData("/color:black//wait:1/[저기 하늘에 떠 다니는 건 뭐지?]", "User"));
-        FirstDialog.Add(new DialogData("/color:black/어디? 어디?", "NoonDung"));
-        FirstDialog.Add(new DialogData("/color:black//wait:1/우리는! /wait:0.5/학교를 지키는 어벤져스, /click/눈꽃송이들이야!", "NoonKkot"));
-        FirstDialog.Add(new DialogData("/color:black/마침 잘 만났다! 얘들아, 이 새송이가 눈송이와 친구가 되고 싶대!", "NoonDung", () => noondungChangeAnimation("stand")));
-        FirstDialog.Add(new DialogData("/color:black/그런거라면... 눈송이는 눈의 결정을 좋아하니까, 눈의 결정을 준다면 분명 친구가 될 수 있을 거야", "NoonKkot"));
-        FirstDialog.Add(new DialogData("/color:black/마침 우리한테 꿍쳐놓은 눈의 결정이 있으니까, 너한테 줄게!", "NoonKkot"));
-        FirstDialog.Add(new DialogData("/color:black/어려운 친구를 돕는 것도 우리 일이니까. 우리가 새송이를 도와주는 건 어떨까? /click/(뭉치면 산다!)", "NoonKkot"));
-        FirstDialog.Add(new DialogData("/color:black/그래, 눈의 결정이라면 우리가 전문이니까, 함께 다니면서 눈의 결정 찾는걸 도와줄게! /click/(맡겨 줘!)", "NoonKkot"));
+        FirstDialog.Add(new DialogData("/color:black/어디? 어디?", "NoonDung", () => StartCoroutine(SnowflakeMove())));
+        FirstDialog.Add(new DialogData("/color:black//wait:1/우리는! /wait:0.5/학교를 지키는 어벤져스, /click/눈꽃송이들이야!", "Snowflake", () => ChangeAnimation(noonDungAnimator, "standing")));
+        FirstDialog.Add(new DialogData("/color:black/마침 잘 만났다! 얘들아, 이 새송이가 눈송이와 친구가 되고 싶대!", "NoonDung"));
+        FirstDialog.Add(new DialogData("/color:black/그런거라면... 눈송이는 눈의 결정을 좋아하니까, 눈의 결정을 준다면 분명 친구가 될 수 있을 거야", "Snowflake"));
+        FirstDialog.Add(new DialogData("/color:black/마침 우리한테 꿍쳐놓은 눈의 결정이 있으니까, 너한테 줄게!", "Snowflake", () => ChangeAnimation(snowflakeAnimator, "standing")));
+        FirstDialog.Add(new DialogData("/color:black/어려운 친구를 돕는 것도 우리 일이니까. 우리가 새송이를 도와주는 건 어떨까? /click/(뭉치면 산다!)", "Snowflake"));
+        FirstDialog.Add(new DialogData("/color:black/그래, 눈의 결정이라면 우리가 전문이니까, 함께 다니면서 눈의 결정 찾는걸 도와줄게! /click/(맡겨 줘!)", "Snowflake"));
         FirstDialog.Add(new DialogData("/color:black/[고마워, 눈꽃송이들!]/wait:1//close/", "User"));
         // part3 로로 등장
-        FirstDialog.Add(new DialogData("/color:black//wait:1/앗! 찾았다", "RoRo", () => StartCoroutine(RoroComing())));
-        FirstDialog.Add(new DialogData("/color:black/[앗!]", "User"));
-        FirstDialog.Add(new DialogData("/color:black/네가 눈송이를 찾아 다닌다는 새송이 맞지! 소식을 듣고 한달음에 달려왔어!", "RoRo", () => RoroChangeAnimation("standing")));
-        FirstDialog.Add(new DialogData("/color:black/로로잖아! 과연 학교의 소식통이라 그런지, 소식이 빠르네!", "NoonDung", () => noondungChangeAnimation("stand")));
-        FirstDialog.Add(new DialogData("/color:black//emote:Happy/히히, 1캠퍼스 정문에서 눈송이를 본 것 같다는 걸 알려주려고! 새송이가 누군지 궁금하기도 했고!", "RoRo", () => RoroChangeAnimation("standing")));
+        FirstDialog.Add(new DialogData("/color:black//wait:1/앗! 찾았다", "RoRo", () => StartCoroutine(RoroMove())));
+        FirstDialog.Add(new DialogData("/color:black/[앗!]", "User", () => ChangeAnimation(roroAnimator, "standing")));
+        FirstDialog.Add(new DialogData("/color:black/네가 눈송이를 찾아 다닌다는 새송이 맞지! 소식을 듣고 한달음에 달려왔어!", "RoRo", () => ChangeAnimation(noonDungAnimator, "standing")));
+        FirstDialog.Add(new DialogData("/color:black/로로잖아! 과연 학교의 소식통이라 그런지, 소식이 빠르네!", "NoonDung", () => ChangeAnimation(roroAnimator, "standing")));
+        FirstDialog.Add(new DialogData("/color:black//emote:Happy/히히, 1캠퍼스 정문에서 눈송이를 본 것 같다는 걸 알려주려고! 새송이가 누군지 궁금하기도 했고!", "RoRo"));
         FirstDialog.Add(new DialogData("/color:black//emote:Call/아직 학교 지리는 잘 모르지? 내가 같이 가줄게!", "RoRo"));
 
         DialogManager.Show(FirstDialog);
     }
 
+    // 첫 번째 대화 데이터 순서
     private void SecondDialog()
     {
         var SecondDialog = new List<DialogData>();
 
+        // part4 눈결이 등장
         SecondDialog.Add(new DialogData("[저기 하늘에 떠 다니는 건 뭐지?]", "User"));
         SecondDialog.Add(new DialogData("저기,, 안녕하세요! 처음 보는 분이네요..!", "NoonGyeol"));
         SecondDialog.Add(new DialogData("눈결이 안녕! 혹시 근처에서 눈송이 못봤어?", "NoonDung"));
@@ -84,17 +91,13 @@ public class TalkDialogue : MonoBehaviour
         FirstDialog();
     }
 
-    private void noondungChangeAnimation(string trigger)
+    // 애니메이션 변경 함수 (애니메이터 이름, trigger 문자열) 받음
+    private void ChangeAnimation(Animator animator, string trigger)
     {
-        noonDungAnimator.SetTrigger(trigger); // 애니메이션 트리거 설정
+        animator.SetTrigger(trigger); // 애니메이션 트리거 설정
     }
 
-    private void RoroChangeAnimation(string trigger)
-    {
-        RoroAnimator.SetTrigger(trigger); // 애니메이션 트리거 설정
-    }
-
-    private IEnumerator NoonDungComing()
+    private IEnumerator NoonDungMove()
     {
         // 시작 위치와 끝 위치 설정
         Vector3 startPos = arCamera.TransformPoint(new Vector3(-1.5f, 1.5f, 5f)); // AR 카메라의 좌측 상단 (화면 좌표계)
@@ -114,7 +117,7 @@ public class TalkDialogue : MonoBehaviour
         }
     }
 
-    private IEnumerator RoroComing()
+    private IEnumerator RoroMove()
     {
         // 시작 위치와 끝 위치 설정
         Vector3 startPos = arCamera.TransformPoint(new Vector3(0f, 0f, 10f)); // AR 카메라의 뒤에서 달려옴
@@ -130,6 +133,26 @@ public class TalkDialogue : MonoBehaviour
             elapsedTime += Time.deltaTime;
             float t = Mathf.Clamp01(elapsedTime / moveDuration);
             roro.transform.position = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+    }
+
+    private IEnumerator SnowflakeMove()
+    {
+        // 시작 위치와 끝 위치 설정
+        Vector3 startPos = arCamera.TransformPoint(new Vector3(0f, 5f, 0f)); // AR 카메라의 위에서 내려옴
+        Vector3 endPos = arCamera.TransformPoint(new Vector3(0f, 0f, 1f)); // AR 카메라의 정면 (화면 중앙)
+
+        // 눈덩이 초기 위치 설정
+        snowflake.transform.position = startPos;
+
+        // 이동 애니메이션 실행
+        float elapsedTime = 0f;
+        while (elapsedTime < moveDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / moveDuration);
+            snowflake.transform.position = Vector3.Lerp(startPos, endPos, t);
             yield return null;
         }
     }
